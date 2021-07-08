3acb5379cd67ea066ecd99d9b3ec2b2f9327388e
Geovani Gomes <geovanisc1@hotmail.com>
2021-07-08 09:24:17 -0300

    resoluçao desafio 7

--- 07_front_back.py
+++ 07_front_back.py
@@ -10,9 +10,37 @@ Exemplo: 'abcde', a metade da frente é 'abc' e a de trás é 'de'.
 Finalmente, dadas duas strings a e b, retorne uma string na forma:
 a-frente + b-frente + a-trás + b-trás
 """
+
+def _definir_metade(string):
+    tamanho = len(string)
+    metade = divmod(tamanho, 2)
+    result = ""
+    primeira_metade = ""
+    segunda_metade = ""
+    if metade[1] == 1:
+        partes = metade[0] + 1
+        primeira_metade = string[:partes]
+        segunda_metade = string[partes:]
+        result = (primeira_metade, segunda_metade)
+        return result
+    else:
+        partes = metade[0]
+        primeira_metade = string[0:partes]
+        segunda_metade = string[partes:]
+        result = (primeira_metade, segunda_metade)
+
+    return result
+
+
+def _misturar_strings(a,b):
+    frase = f"{a[0]}{b[0]}{a[1]}{b[1]}"
+    return frase
+
 def front_back(a, b):
     # +++ SUA SOLUÇÃO +++
-    return
+    result_a = _definir_metade(a)
+    result_b = _definir_metade(b)
+    return _misturar_strings(result_a, result_b)
 
 
 # --- Daqui para baixo são apenas códigos auxiliáries de teste. ---
